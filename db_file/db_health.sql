2c4705d4f65b6fbca0da34986949952b1d07c0d0
Yasin Muhammed Tukur <saukakke@gmail.com>
2021-06-26 16:58:23 -0700

    Added database folder and file

---
+++ db_file/db_health.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2021 at 11:41 PM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_health`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_feedback`
+--
+
+CREATE TABLE `tbl_feedback` (
+  `message` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_feedback`
+--
+
+INSERT INTO `tbl_feedback` (`message`) VALUES
+('hi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_hospital`
+--
+
+CREATE TABLE `tbl_hospital` (
+  `name` varchar(100) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `image` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_hospital`
+--
+
+INSERT INTO `tbl_hospital` (`name`, `address`, `city`, `state`, `email`, `image`) VALUES
+('saukakke', 'saukakke', 'saukakke', 'saukakke', 'saukakke@gmail.com', '../upload/Screenshot (8).png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_location`
+--
+
+CREATE TABLE `tbl_location` (
+  `address` varchar(200) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `postal_code` varchar(20) NOT NULL,
+  `name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_location`
+--
+
+INSERT INTO `tbl_location` (`address`, `city`, `state`, `email`, `postal_code`, `name`) VALUES
+('saukakke', 'saukakke', 'saukakke', 'saukakke@gmail.com', '4', 'saukakke'),
+('saukakke', 'saukakke', 'saukakke', 'saukakke@gmail.com', '222', 'saukakke');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_profile`
+--
+
+CREATE TABLE `tbl_profile` (
+  `name` varchar(25) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `state` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `issue` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_profile`
+--
+
+INSERT INTO `tbl_profile` (`name`, `gender`, `location`, `state`, `email`, `issue`) VALUES
+('saukakke', 'male', 'samaru', 'state', 'saukakke@gmail.com', 'nil');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
